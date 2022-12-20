b5eefc9ea3dc04fc123c4a522557397792e98867
Thomas Clement <thocle86@gmail.com>
2022-12-20 13:15:05 +0100

    Ajout d'animation

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -1,11 +1,32 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Comment } from 'src/app/core/models/comment.model';
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
-  styleUrls: ['./comments.component.scss']
+  styleUrls: ['./comments.component.scss'],
+  animations: [
+    trigger('listItem', [
+      state('default', style({
+        transform: 'scale(1)',
+        'background-color': 'white',
+        'z-index': 1
+      })),
+      state('active', style({
+        transform: 'scale(1.05)',
+        'background-color': 'rgb(201, 157, 242)',
+        'z-index': 2
+      })),
+      transition('default => active', [
+        animate('100ms ease-in-out')
+      ]),
+      transition('active => default', [
+        animate('500ms ease-in-out')
+      ]),
+    ])
+  ]
 })
 export class CommentsComponent implements OnInit {
 
@@ -14,10 +35,15 @@ export class CommentsComponent implements OnInit {
 
   commentControl!: FormControl;
 
+  animationStates: { [key: number]: 'default' | 'active' } = {};
+
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.commentControl = this.formBuilder.control('', [Validators.required, Validators.minLength(10)]);
+    for (let i in this.commentList) {
+      this.animationStates[i] = 'default';
+    }
   }
 
   onLeaveComment(): void {
@@ -27,4 +53,13 @@ export class CommentsComponent implements OnInit {
     this.newComment.emit(this.commentControl.value);
     this.commentControl.reset();
   }
+
+  onListItemMouseEnter(index: number) {
+    this.animationStates[index] = 'active';
+  }
+
+  onListItemMouseLeave(index: number) {
+    this.animationStates[index] = 'default';
+  }
+
 }
