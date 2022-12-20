3cc2c49d46e7ded51a79edfe60d228702750f0e1
Thomas Clement <thocle86@gmail.com>
2022-12-20 11:28:57 +0100

    Création d'une directive

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,13 +6,15 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { ShortenPipe } from './pipes/shorten.pipe';
 import { UsernamePipe } from './pipes/username.pipe';
 import { TimeAgoPipe } from './pipes/time-ago.pipe';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
     CommentsComponent,
     ShortenPipe,
     UsernamePipe,
-    TimeAgoPipe
+    TimeAgoPipe,
+    HighlightDirective
   ],
   imports: [
     CommonModule,
@@ -25,7 +27,8 @@ import { TimeAgoPipe } from './pipes/time-ago.pipe';
     ReactiveFormsModule,
     ShortenPipe,
     UsernamePipe,
-    TimeAgoPipe
+    TimeAgoPipe,
+    HighlightDirective
   ]
 })
 export class SharedModule { }
