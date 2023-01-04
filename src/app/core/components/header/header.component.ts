6e1cbe820852719fe2cefaae6462b35c5b41fe40
thocle86 <thocle86@gmail.com>
2023-01-04 12:11:28 +0100

    Ajout de 4 styles light et dark

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  
+  currentTheme!: string;
+
+  ngOnInit() {
+  }
+
+  selectDefault() {
+    document.body.classList.remove(this.currentTheme);
+  }
+
+  selectTheme(theme: string) {
+    document.body.classList.remove(this.currentTheme);
+    this.currentTheme = theme;
+    document.body.classList.add(this.currentTheme);
+    console.log(this.currentTheme);
+  }
 
 }
