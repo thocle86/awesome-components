4243aacff74e98e4f516a1b0851ba6b25118271b
thocle86 <thocle86@gmail.com>
2023-01-04 15:36:10 +0100

    Création de la fonction pour le theme dark

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  isDark!: boolean;
+
+  ngOnInit(): void {
+    this.isDark = false;
+  }
+
+  toggleDarkTheme(): void {
+    console.log(this.isDark);
+    document.body.classList.toggle('dark-theme');
+    this.isDark = !this.isDark;
+  }
 
 }
