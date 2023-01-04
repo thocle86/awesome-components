7efc30fcbc240ac11fa3eb2b6a319de4eca960a2
thocle86 <thocle86@gmail.com>
2023-01-04 16:29:47 +0100

    Ajout des themes et palettes et du toggle

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  isDark!: boolean;
+  currentTheme!: string;
+
+  ngOnInit(): void {
+    this.isDark = false;
+  }
+
+  toggleDarkTheme(): void {
+    console.log(this.isDark);
+    document.body.classList.toggle('dark-theme');
+    this.isDark = !this.isDark;
+  }
+
+  selectDefault() {
+    document.body.classList.remove(this.currentTheme);
+  }
+
+  selectTheme(theme: string) {
+    document.body.classList.remove(this.currentTheme);
+    this.currentTheme = theme;
+    document.body.classList.add(this.currentTheme);
+    console.log(this.currentTheme);
+  }
 
 }
